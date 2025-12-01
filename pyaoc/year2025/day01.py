1412085a1b49b5fa3fec2b9cc629ce346cab4440
Ian Robinson <ianronrobinson@gmail.com>
2025-12-01 15:39:43 -0500

    2025 day 1

--- pyaoc/year2025/day01.py
+++ pyaoc/year2025/day01.py
@@ -1,6 +1,29 @@
 def part1(puzzle_input: str) -> None:
-    print("Not yet implemented")
+    dial = 50
+    zero_count = 0
+    for line in puzzle_input.splitlines():
+        direction = line[0]
+        count = int(line[1:])
+        if direction == "L":
+            dial -= count
+        elif direction == "R":
+            dial += count
+        dial %= 100
+        if dial == 0:
+            zero_count += 1
+    print(zero_count)
 
 
 def part2(puzzle_input: str) -> None:
-    print("Not yet implemented")
+    dial = 50
+    zero_count = 0
+    for line in puzzle_input.splitlines():
+        direction = line[0]
+        count = int(line[1:])
+        click = -1 if direction == "L" else 1
+        for _ in range(count):
+            dial += click
+            dial %= 100
+            if dial == 0:
+                zero_count += 1
+    print(zero_count)
